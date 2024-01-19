9104d5706d9c446e23bca27343794bee9cabb61c
Giuseppe Leocata <peppeleocata@gmail.com>
2024-01-19 17:56:18 +0100

    Create Cliente.java

---
+++ pawbookings/src/main/java/doamin_layer/Cliente.java
@@ -0,0 +1,19 @@
+package doamin_layer;
+
+public class Cliente {
+    private int codiceCliente;
+    private String nome;
+    private String cognome;
+
+    public int getCodiceCliente() {
+        return this.codiceCliente;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getCognome() {
+        return this.cognome;
+    }
+}
