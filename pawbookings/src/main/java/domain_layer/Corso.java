a3658dd5eb8a7e04e3f13ed0da71042e56bc5305
Daniele Lucifora <111700564+danielux86@users.noreply.github.com>
2024-01-20 11:46:17 +0100

    Create Corso.java

---
+++ pawbookings/src/main/java/domain_layer/Corso.java
@@ -0,0 +1,20 @@
+package domain_layer;
+
+public class Corso {
+    private int codice;
+    private int capienza;
+    private float costo;
+    private String tipoCorso;
+
+    public void confermaIscrizione() {
+
+    }
+
+    public void aggiornaCapienza() {
+
+    }
+
+    public void aggiornaCaniIscritti() {
+
+    }
+}
