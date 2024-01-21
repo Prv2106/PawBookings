99a3b579df3e4b82ad2716588bd8ca245e1a6e47
Daniele Lucifora <111700564+danielux86@users.noreply.github.com>
2024-01-21 17:25:40 +0100

    Update PawBookingsTest.java

--- pawbookings/src/test/java/com/example/PawBookingsTest.java
+++ pawbookings/src/test/java/com/example/PawBookingsTest.java
@@ -2,20 +2,37 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import domain_layer.Cliente;
+import domain_layer.PawBookings;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
 
 class PawBookingsTest {
 
     @Test
-    void getInstance() {
+    void testGetInstance() {
+        PawBookings PB = PawBookings.getInstance();
+        assertNotNull(PB);
+    }
+
+    // Il metodo crea con successo tre istanze della classe 'Cliente'.
+    @Test
+    void testLoadClienti() {
+        PawBookings PB = PawBookings.getInstance();
+        PB.loadClienti();
+        Map<Integer, Cliente> clienti = PB.getClienti();
+    
+        assertEquals(3, clienti.size());
+        assertTrue(clienti.containsKey(1));
+        assertTrue(clienti.containsKey(2));
+        assertTrue(clienti.containsKey(3));
     }
 
     @Test
-    void loadClienti() {
-    }
-
-    @Test
-    void loadCorsi() {
+    void testLoadCorsi() {
+        
     }
 
     @Test
