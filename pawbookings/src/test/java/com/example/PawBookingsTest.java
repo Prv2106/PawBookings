a943904cfd43d8803b79c7f91aa52032cb95e7b9
Daniele Lucifora <111700564+danielux86@users.noreply.github.com>
2024-01-21 17:09:36 +0100

    Creazione Test metodi PawBookings

--- pawbookings/src/test/java/com/example/PawBookingsTest.java
+++ pawbookings/src/test/java/com/example/PawBookingsTest.java
@@ -2,9 +2,47 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PawBookingsTest {
 
     @Test
+    void getInstance() {
+    }
+
+    @Test
+    void loadClienti() {
+    }
+
+    @Test
+    void loadCorsi() {
+    }
+
+    @Test
+    void nuovaIscrizioneCorso() {
+    }
+
+    @Test
+    void confermaIscrizioneCorso() {
+    }
+
+    @Test
+    void prenotaTurnoLezione() {
+    }
+
+    @Test
+    void selezionaTurno() {
+    }
+
+    @Test
     void selezionaCane() {
+    }
+
+    @Test
+    void getClienti() {
+    }
+
+    @Test
+    void getCaneSelezionato() {
     }
 }
