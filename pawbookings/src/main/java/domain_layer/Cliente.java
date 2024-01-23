e5c49f895a04be5908121da7a0fbd79cc1f5b0d0
Daniele Lucifora <111700564+danielux86@users.noreply.github.com>
2024-01-23 17:07:53 +0100

    Aggiunta metodi iterazione 2 alle classi Cliente e Cane

--- pawbookings/src/main/java/domain_layer/Cliente.java
+++ pawbookings/src/main/java/domain_layer/Cliente.java
@@ -3,19 +3,58 @@ package domain_layer;
 import java.util.LinkedList;
 
 public class Cliente {
-    private int codiceCliente;
+    private String codiceCliente;
     private String nome;
     private String cognome;
     private LinkedList<Cane> caniPosseduti;
 
-    public Cliente (int codiceCliente, String nome, String cognome) {
+    public Cliente (String codiceCliente, String nome, String cognome) {
         this.codiceCliente = codiceCliente;
         this.nome = nome;
         this.cognome = cognome;
         this.caniPosseduti = new LinkedList<>();
     }
 
-    public int getCodiceCliente() {
+    // public void loadCani(int codiceCane, String nome, String razza){
+    //     Cane cn = new Cane(codiceCane, nome, razza);
+    //     this.caniPosseduti.add(cn);
+    // }
+
+    public LinkedList<Cane> getCaniNonInAffido() {
+        return this.calcolaCaniNonInAffido();
+    }
+
+    //Il metodo restituisce una lista di cani non in affido.
+    public LinkedList<Cane> calcolaCaniNonInAffido() {
+        LinkedList<Cane> elencoCaniNonInAffido = new LinkedList<>();
+        for (Cane cn : this.caniPosseduti) {
+            if (!cn.attualmenteInAffido) {
+                elencoCaniNonInAffido.add(cn);
+            }
+        }
+        return elencoCaniNonInAffido;
+    }
+
+    // Il metodo registra un cane, aggiungendolo alla lista dei cani posseduti dal cliente.
+    public boolean registraCane(String nome, String razza){
+        int codCane = generaCodiceCane();
+        Cane cn = new Cane(codCane, nome, razza);
+        caniPosseduti.add(cn);
+        return true;
+    }
+
+    // Il metodo genera un codice cane, che è il risultato della somma tra il numero di cani posseduti e 1.
+    public int generaCodiceCane(){
+        int codiceCane = 0;
+        for (Cane cn : this.caniPosseduti) {
+            codiceCane = this.caniPosseduti.size() + 1;
+        }
+        return codiceCane;
+    }
+
+    // Metodi per recuperare gli attributi
+
+    public String getCodiceCliente() {
         return this.codiceCliente;
     }
 
@@ -30,13 +69,5 @@ public class Cliente {
     public LinkedList<Cane> getCani() {
         return this.caniPosseduti;
     }
-
-
-    public void loadCani(int codiceCane, String nome, String razza){
-        Cane cn = new Cane(codiceCane, nome, razza);
-        this.caniPosseduti.add(cn);
-    }
-
-
 
 }
