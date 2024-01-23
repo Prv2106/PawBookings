6adca916291215f6964da5c50bf08a17f4ee9fcd
Giuseppe Leocata <peppeleocata@gmail.com>
2024-01-23 16:15:45 +0100

    aggiunta classe Affido

---
+++ pawbookings/src/main/java/domain_layer/PeriodoAffido.java
@@ -0,0 +1,43 @@
+import java.util.Date;
+import java.util.LinkedList;
+
+public class PeriodoAffido {
+    // attributi
+    private final int codice;
+    private final Date dataInizio;
+    private final Date dataFine;
+    private final float costo;
+    private int numPosti; // derivato, sono i posti disponibili
+    private int CapienzaMassima;
+
+    // riferimenti
+    private LinkedList<Cane> elencoCaniAffido;
+
+    public PeriodoAffido(int codice, Date dataInizio, Date dataFine, float costo) {
+        this.codice = codice;
+        this.dataInizio = dataInizio;
+        this.dataFine = dataFine;
+        this.costo = costo;
+        this.CapienzaMassima = 5;
+        this.numPosti = this.CapienzaMassima;
+    }
+
+    public boolean registraAffido(Cane cn) {
+        if (this.elencoCaniAffido.add(cn)) {
+            aggiornaNumeroPosti();
+            return true;
+        } else 
+            return false;
+    }    
+
+    public boolean concludiAffido(Cane cn) {
+        if (this.elencoCaniAffido.remove(cn))
+            return true;
+        else 
+            return false;        
+    }
+
+    public void aggiornaNumeroPosti() {
+        this.numPosti--;
+    }
+}
