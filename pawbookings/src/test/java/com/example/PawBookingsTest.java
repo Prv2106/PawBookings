efe6a4d4c2bb87c1c57d8d063a64d98771d2b69b
Alberto Provenzano <provi.al2106@gmail.com>
2024-01-24 13:02:02 +0100

    Aggiornamento Test della classe PawBookings

--- pawbookings/src/test/java/com/example/PawBookingsTest.java
+++ pawbookings/src/test/java/com/example/PawBookingsTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import domain_layer.Cane;
-import domain_layer.Cliente;
 import domain_layer.Corso;
 import domain_layer.CorsoBase;
 import domain_layer.PawBookings;
@@ -12,7 +11,7 @@ import domain_layer.Turno;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Map;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.LinkedList;
@@ -21,6 +20,8 @@ class PawBookingsTest {
 
     static PawBookings PB;
 
+    /***    TEST PER ITERAZIONE 1   ***/
+
     // Il metodo viene eseguito prima di tutti i test
     // inizializza l'istanza di PawBookings
     // crea un'istanza di Cane e la seleziona
@@ -28,6 +29,8 @@ class PawBookingsTest {
     public static void initTest() {
         PB = PawBookings.getInstance();
         PB.selezionaCane(new Cane(1, "Luna", "Barboncino"));
+        PB.loadCorsi();
+        PB.getCaneSelezionato().aggiornaAttualmenteIscritto(PB.elencoCorsi.get(0));
     }
 
     // Il metodo restituisce un'istanza della classe PawBookings se PB è null.
@@ -36,38 +39,17 @@ class PawBookingsTest {
         assertNotNull(PB);
     }
 
-    // Il metodo crea con successo tre istanze della classe Cliente.
-    @Test
-    void testLoadClienti() {
-        PB.loadClienti();
-        Map<Integer, Cliente> clienti = PB.getClienti();
+
     
-        assertEquals(3, clienti.size());
-        assertTrue(clienti.containsKey(1));
-        assertTrue(clienti.containsKey(2));
-        assertTrue(clienti.containsKey(3));
-    }
-
-    @Test
-    void testLoadCorsi() {
-        // il costruttore della classe singleton PB chiama il metodo "loadCorsi"
-        // il quale crea 3 corsi, pertanto verifichiamo che la lunghezza combacia
-        LinkedList<Corso> elencoCorsi = PB.elencoCorsi;
-
-        assertEquals(3, elencoCorsi.size());
-    }
-
     // Test Whitebox
     // Il metodo verifica che la lista dei corsi disponibili contenga corsi 
     // la cui capienza sia effettivamente maggiore di 0
     void testNuovaIscrizioneCorso() {
         LinkedList<Corso> elencoCorsi = new LinkedList<>();
         elencoCorsi = PB.nuovaIscrizioneCorso();
-    
         for (Corso corso : elencoCorsi) {
             assertTrue(corso.getCapienza() > 0);
         }
-
     }
 
     @Test
@@ -94,6 +76,8 @@ class PawBookingsTest {
         assertNotNull(PB.getCaneSelezionato());
         assertEquals("Luna", PB.getCaneSelezionato().getNome());
     }
+
+
 
   
 }
