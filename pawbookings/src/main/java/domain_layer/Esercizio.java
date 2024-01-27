a7058c77d50dde897bc8d61933aac6f84a8ae022
Daniele Lucifora <111700564+danielux86@users.noreply.github.com>
2024-01-27 18:19:49 +0100

    Implementazione classi Corso, Esercizio, Lezione e PeriodoAffido
    Relativamente all'iterazione 3, sono stati aggiunti i seguenti metodi:
    - aggiornaInformazioni()
    - nuovaLezione()
    - setLezioneCorrente()
    - aggiornaLezione()
    - confermaInserimentoLezione()
    - generaCodice()
    - calcolaStatoAvanzamento()
    - nuovoEsercizio()
    - nuovoTurno()
    - effettuaScambioTurno()
    - verificaIscrizione()
    - aggiornaStatoSalute()
    - getState()

---
+++ pawbookings/src/main/java/domain_layer/Esercizio.java
@@ -0,0 +1,11 @@
+package domain_layer;
+
+public class Esercizio {
+    private String nome;
+    private String descrizione;
+
+    public Esercizio(String nome, String descrizione) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+}
